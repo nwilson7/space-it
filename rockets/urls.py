6925d5f478b2101b72a22f9ea1a850dc9e6ac1af
Sean Graham <seangraham006@gmail.com>
2025-01-31 15:54:29 +0000

    update to rocket urls

--- rockets/urls.py
+++ rockets/urls.py
@@ -2,8 +2,8 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('view_rockets',views.view_rockets, name='view_rockets'),
-    path('add_rocket/',views.add_rocket, name='add_rocket'),
-    path('edit_rocket/<int:id>/', views.edit_rocket, name='edit_rocket'),
-    path('delete_rocket/<int:id>/', views.delete_rocket, name='delete_rocket'),
+    path('',views.view_rockets, name='view_rockets'),
+    path('add/',views.add_rocket, name='add_rocket'),
+    path('edit/<int:id>/', views.edit_rocket, name='edit_rocket'),
+    path('delete/<int:id>/', views.delete_rocket, name='delete_rocket'),
 ]
