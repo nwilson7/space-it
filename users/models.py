aabcbb885d4d8fb328db5dfb54f0be3fcfcfe132
Blacknova7 <87141874+iBlacknova@users.noreply.github.com>
2025-02-04 13:26:02 +0000

    refactors rocket/views.py to use request.user

--- users/models.py
+++ users/models.py
@@ -11,4 +11,4 @@ class User(AbstractUser):
     role = models.CharField(max_length=12, choices=ROLE_CHOICES)
 
     def __str__(self):
-        return f"{self.username} ({self.role})"
+        return f"Hi {self.username} (ID: {self.id}) ({self.role})"
