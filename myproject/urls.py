0e4c2e579dad1e740ce008282e6b0c0e21817350
Blacknova7 <87141874+iBlacknova@users.noreply.github.com>
2025-02-04 11:50:49 +0000

    adds relevant migrations and setup files for each app

--- myproject/urls.py
+++ myproject/urls.py
@@ -3,12 +3,8 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-<<<<<<< Updated upstream
-    path('',include('myapp.urls')),
+    path('',include('users.urls')),
     path('users/',include('users.urls')),
     path('rockets/',include('rockets.urls')),
     path('cargo/',include('cargo.urls')),
-=======
-    path('',include('users.urls')),
->>>>>>> Stashed changes
 ]
