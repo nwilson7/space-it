8e1917532e738ffc8071675c76837090e2080edd
Blacknova7 <87141874+iBlacknova@users.noreply.github.com>
2025-02-13 13:05:28 +0000

    adds generic styling to signup page

--- users/forms.py
+++ users/forms.py
@@ -7,9 +7,33 @@ class CustomLoginForm(forms.Form):
     password = forms.CharField(widget=forms.PasswordInput)
 
 class CustomUserCreationForm(UserCreationForm):
+    username = forms.CharField(
+        max_length=100,
+        widget=forms.TextInput(attrs={'id': 'username-input'}),
+        required=True
+    )
+    email = forms.EmailField(
+        widget=forms.EmailInput(attrs={'id': 'email-input'}),
+        required=True
+    )
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(attrs={'id': 'password1-input'}),
+        required=True
+    )
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(attrs={'id': 'password2-input'}),
+        required=True
+    )
+    role = forms.ChoiceField(
+        choices=User.ROLE_CHOICES,  # Assuming your User model has a ROLE_CHOICES field
+        widget=forms.Select(attrs={'id': 'role-select'}),
+        required=True
+    )
+
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2', 'role']
+        fields = ['username', 'email', 'role', 'password1', 'password2']
+
 
 class UsernameUpdateForm(forms.ModelForm):
     class Meta:
